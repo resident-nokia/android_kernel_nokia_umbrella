704c1565bb4e3ccd2c704f07ca3ef163a6a088bd
Dorian Stoll <dorian.stoll@tmsp.io>
2019-09-28 21:19:43 +0200

    Update to AnyKernel3
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- umbrella/template/anykernel.sh
+++ umbrella/template/anykernel.sh
@@ -1,4 +1,4 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
@@ -20,12 +20,11 @@ ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
+
 
 ## AnyKernel install
-
 dump_boot;
 write_boot;
-
 ## end install
 
