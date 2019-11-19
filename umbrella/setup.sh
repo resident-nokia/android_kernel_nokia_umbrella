02b8ff885441ac43429c3af85818453d0548c06f
Dorian Stoll <dorian.stoll@tmsp.io>
2019-11-19 20:05:59 +0100

    Update to GCC 9.2 and add toolchain as submodule
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- umbrella/setup.sh
+++ umbrella/setup.sh
@@ -4,17 +4,12 @@
 export ARCH="arm64"
 export SUBARCH="arm64"
 
-# Fetch the toolchain
-if [ ! -d prebuilt/gcc-arm-8.3 ]; then
-    wget -O gcc-arm-8.3.tar.xz https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz
-    tar xJvf gcc-arm-8.3.tar.xz
-    if [ ! -d prebuilt ]; then
-        mkdir prebuilt
-    fi
-    mv gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu prebuilt/gcc-arm-8.3
-    rm gcc-arm-8.3.tar.xz
-fi
-export CROSS_COMPILE="$(pwd)/prebuilt/gcc-arm-8.3/bin/aarch64-linux-gnu-"
+# Update submodules
+git submodule update --recursive --init
+
+# Detect the toolchain
+TRIPLET=$(basename $(pwd)/toolchain/bin/*gcc | sed 's|gcc$||g')
+export CROSS_COMPILE="$(pwd)/toolchain/bin/$TRIPLET"
 
 # Set the output directory
 export KBUILD_OUTPUT="$(pwd)/out"
@@ -28,15 +23,13 @@ alias umbrella-commit='git rev-parse --verify --short HEAD'
 alias umbrella-tag='git describe --exact-match --abbrev=0'
 
 # Nice output
-echo "Build Environment initialized:"
-echo "  ARCH: $ARCH"
-echo "  KBUILD_OUTPUT: $KBUILD_OUTPUT"
-echo ""
+$(pwd)/toolchain/bin/${TRIPLET}gcc --version
+
 echo "To configure the build for NB1, type"
-echo "  make nb1_defconfig"
+echo "  make -j$(nproc --all) nb1_defconfig"
 echo ""
 echo "To configure the build for A1N, type"
-echo "  make a1n_defconfig"
+echo "  make -j$(nproc --all) a1n_defconfig"
 echo ""
 echo "To compile the configured kernel, type"
 echo "  make -j$(nproc --all) LOCALVERSION=-\$(umbrella-commit)"
