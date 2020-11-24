889bdb584fe3592f373a49d26292197318832e81
Radu Vasilascu <glx33807@gmail.com>
2020-11-24 19:07:54 +0000

    Remove the _Treble suffix from Anykernel

--- umbrella/template/anykernel.sh
+++ umbrella/template/anykernel.sh
@@ -10,7 +10,7 @@ do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1={DEVICE}_Treble
+device.name1={DEVICE}
 supported.versions=
 supported.patchlevels=
 '; } # end properties
